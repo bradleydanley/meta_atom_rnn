37308dd3a7c64754af15196fcb57002caad561f3
agkgd4 <kellerandreag@gmail.com>
2024-05-06 17:19:21 -0500

    d

--- preprocess_data/preprocess.py
+++ preprocess_data/preprocess.py
@@ -38,8 +38,6 @@ def run(params):
     exclude = []
     i = 0
     #from IPython import embed; embed(); exit()
-    print("end of test")
-    exit()
     with os.scandir(path_volumes) as entries:
 
         for entry in entries:
