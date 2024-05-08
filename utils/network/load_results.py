95c846fbdb63885be2aaf4bde1ec07fb6dfbab30
agkgd4 <kellerandreag@gmail.com>
2024-05-07 19:22:17 -0500

    h

--- utils/network/load_results.py
+++ utils/network/load_results.py
@@ -120,7 +120,7 @@ def organize_results(params):
 def run(params):
 
     path_results = params['paths']['results']
-    path_results = 
+    #path_results =  
     create_folder(os.path.join(path_results 'analysis'))
     
     create_folder(os.path.join(params['paths']['results'], 'analysis'))
