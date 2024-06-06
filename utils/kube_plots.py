a9f83205fe379c2b4967a102c08c300408d27487
mindful <bbobbradley@gmail.com>
2024-06-06 15:53:47 -0500

    kube eval test 9

--- utils/kube_plots.py
+++ utils/kube_plots.py
@@ -413,7 +413,7 @@ def plot_loss(params, all_data, path_results, y_lim=[0,0.18], figsize=(10, 5), f
     exclude_group = params['visualize']['exclude_group']
     sequences = params['visualize']['sequences']
     for some_data in all_data:
-        for val, all_loss in all_data.items(): 
+        for val, all_loss in some_data.items(): 
 
             if val not in sequences:
                 continue
